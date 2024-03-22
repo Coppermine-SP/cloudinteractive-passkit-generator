a844054e4ded79093d00899b29d5f97a74effb13
Coppermine-SP <son5121502@gmail.com>
2024-03-22 22:03:39 +0900

    add: IssueController

---
+++ src/Controllers/IssueController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Cloudinteractive.PassKitGenerator.Models;
+
+namespace Cloudinteractive.PassKitGenerator.Controllers
+{
+    public class IssueController : Controller
+    {
+        private readonly ILogger<IssueController> _logger;
+
+        public IssueController(ILogger<IssueController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
