976be9dc000f0e49c9a7023dc6679b2f5b2a655e
Coppermine-SP <son5121502@gmail.com>
2024-03-24 23:45:13 +0900

    design: Error 페이지 디자인 재구성

--- src/Models/ErrorViewModel.cs
+++ src/Models/ErrorViewModel.cs
@@ -2,8 +2,7 @@ namespace Cloudinteractive.PassKitGenerator.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public string ErrorId { get; set; }
+        public string Message { get; set; }
     }
 }
